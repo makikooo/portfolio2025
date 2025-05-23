e1cce7e3ffdfeebfc322d87c2047e26485224abb
MAKIKO KARASAWA <truehope320@gmail.com>
2025-05-23 21:10:44 +0900

    つづきを見るの下を非表示にする（スマホ表示）

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,28 +39,33 @@ humbergur.addEventListener('click', function () {
 /*******************************************
  *  トップページ　ワークのつづきを見る
  *******************************************/
-let work_continuation = document.querySelector('.work_continuation');
-let continuation_button = document.querySelector('.continuation_button');
-let continuation_button_wrapper = document.querySelector('.center_wrapper');
+(() => {
+    let work_continuation = document.querySelector('.work_continuation');
+    let continuation_button = document.querySelector('.continuation_button');
+    let continuation_button_wrapper = document.querySelector('.center_wrapper');
 
-continuation_button.addEventListener('click', function () {
-    work_continuation.classList.add('active');
-    continuation_button_wrapper.classList.add('hidden');
-});
+    continuation_button.addEventListener('click', function () {
+        work_continuation.classList.add('active');
+        continuation_button_wrapper.classList.add('hidden');
+    });
+})();
 
 /*******************************************
  *  トップページ　ワークのつづきを見る2個目
  *******************************************/
-let work_link_wrapper_hidden = document.querySelectorAll('.work_link_wrapper.hidden');
-let continuation_button02 = document.querySelector('.continuation_button02');
-let continuation_button_wrapper02 = document.querySelector('.center_wrapper02');
+(() => {
 
-continuation_button02.addEventListener('click', function () {
-    work_link_wrapper_hidden.forEach(function (element) {
-        element.classList.remove('hidden');
+    let work_link_wrapper_hidden = document.querySelectorAll('.work_link_wrapper.hidden');
+    let continuation_button02 = document.querySelector('.continuation_button02');
+    let continuation_button_wrapper02 = document.querySelector('.center_wrapper02');
+
+    continuation_button02.addEventListener('click', function () {
+        work_link_wrapper_hidden.forEach(function (element) {
+            element.classList.remove('hidden');
+        });
+        continuation_button_wrapper02.classList.add('hidden');
     });
-    continuation_button_wrapper02.classList.add('hidden');
-});
+})();
 
 /*******************************************
  *  各ページ(フッター）上に戻るのボタンを押したら時間かけて戻る
